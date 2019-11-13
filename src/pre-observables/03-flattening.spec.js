1bb29cd52e591369716b4a15736aca2c11139da1
wpannell <wil.pannell@gmail.com>
2019-11-13 06:16:48 -0500

    Array.zip

--- src/pre-observables/03-flattening.spec.js
+++ src/pre-observables/03-flattening.spec.js
@@ -210,6 +210,17 @@ describe(`flattening `, () => {
         },
       ])
   })
+
+  it.only('zips', () => {
+    Array.zip(videos_, bookmarks, (x, y) => ({
+      videoId: x.id,
+      bookmarkId: y.id,
+    })).should.deepEqual([
+      {videoId: 70111470, bookmarkId: 470},
+      {videoId: 654356453, bookmarkId: 453},
+      {videoId: 65432445, bookmarkId: 445},
+    ])
+  })
 })
 
 const secondMovieLists = [
@@ -486,6 +497,54 @@ const videos = [
     title: 'Bad Boys',
   },
 ]
+
+const videos_ = [
+  {
+    id: 70111470,
+    title: 'Die Hard',
+    boxart: 'http://cdn-0.nflximg.com/images/2891/DieHard.jpg',
+    uri: 'http://api.netflix.com/catalog/titles/movies/70111470',
+    rating: 4.0,
+  },
+  {
+    id: 654356453,
+    title: 'Bad Boys',
+    boxart: 'http://cdn-0.nflximg.com/images/2891/BadBoys.jpg',
+    uri: 'http://api.netflix.com/catalog/titles/movies/70111470',
+    rating: 5.0,
+  },
+  {
+    id: 65432445,
+    title: 'The Chamber',
+    boxart: 'http://cdn-0.nflximg.com/images/2891/TheChamber.jpg',
+    uri: 'http://api.netflix.com/catalog/titles/movies/70111470',
+    rating: 4.0,
+  },
+  {
+    id: 675465,
+    title: 'Fracture',
+    boxart: 'http://cdn-0.nflximg.com/images/2891/Fracture.jpg',
+    uri: 'http://api.netflix.com/catalog/titles/movies/70111470',
+    rating: 5.0,
+  },
+]
+
+const bookmarks = [
+  {id: 470, time: 23432},
+  {id: 453, time: 234324},
+  {id: 445, time: 987834},
+]
+
+Array.zip = function(left, right, combinerFunction) {
+  let counter,
+    results = []
+
+  for (counter = 0; counter < Math.min(left.length, right.length); counter++) {
+    results = results.concat(combinerFunction(left[counter], right[counter]))
+  }
+
+  return results
+}
 
 Array.prototype.reduce_ = function(combiner, initialValue) {
   let counter, accumulatedValue
