697f94cec25384f066728c5b672416019defcfd7
wpannell <wil.pannell@gmail.com>
2020-02-26 11:44:06 -0800

    nest amountFor

--- src/martin-fowler-movie-example-2/statement.js
+++ src/martin-fowler-movie-example-2/statement.js
@@ -1,30 +1,30 @@
-function amountFor(aPerformance, play) {
-  let result = 0
-
-  switch (play.type) {
-    case 'tragedy':
-      result = 40000
-      if (aPerformance['audience'] > 30) {
-        result += 1000 * (aPerformance['audience'] - 30)
-      }
-      break
-    case 'comedy':
-      result = 30000
-      if (aPerformance['audience'] > 20) {
-        result += 10000 + 500 * (aPerformance['audience'] - 20)
-      }
-      result += 300 * aPerformance['audience']
-      break
-    default:
-      throw new Error(`unknown type: ${play.type}`)
-  }
-
-  return result
-}
-
 export function statement(invoice, plays) {
   function playFor(perf) {
     return plays[perf['playID']]
+  }
+
+  function amountFor(aPerformance, play) {
+    let result = 0
+
+    switch (play.type) {
+      case 'tragedy':
+        result = 40000
+        if (aPerformance['audience'] > 30) {
+          result += 1000 * (aPerformance['audience'] - 30)
+        }
+        break
+      case 'comedy':
+        result = 30000
+        if (aPerformance['audience'] > 20) {
+          result += 10000 + 500 * (aPerformance['audience'] - 20)
+        }
+        result += 300 * aPerformance['audience']
+        break
+      default:
+        throw new Error(`unknown type: ${play.type}`)
+    }
+
+    return result
   }
 
   let totalAmount = 0
