eb6a3744a4666b5f10cf9854c3969ef857953e1a
wpannell <wil.pannell@gmail.com>
2020-02-26 11:27:11 -0800

    purge lint violations

--- src/martin-fowler-movie-example-2/statement.js
+++ src/martin-fowler-movie-example-2/statement.js
@@ -1,7 +1,7 @@
 export function statement(invoice, plays) {
   let totalAmount = 0
   let volumeCredits = 0
-  let result = `Statement for ${invoice.customer}\n`
+  let result = `Statement for ${invoice['customer']}\n`
 
   const format = new Intl.NumberFormat('en-US', {
     style: 'currency',
@@ -9,36 +9,37 @@ export function statement(invoice, plays) {
     minimumFractionDigits: 2,
   }).format
 
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID]
+  for (let perf of invoice['performances']) {
+    const play = plays[perf['playID']]
     let thisAmount = 0
 
     switch (play.type) {
       case 'tragedy':
         thisAmount = 40000
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30)
+        if (perf['audience'] > 30) {
+          thisAmount += 1000 * (perf['audience'] - 30)
         }
         break
       case 'comedy':
         thisAmount = 30000
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20)
+        if (perf['audience'] > 20) {
+          thisAmount += 10000 + 500 * (perf['audience'] - 20)
         }
-        thisAmount += 300 * perf.audience
+        thisAmount += 300 * perf['audience']
         break
       default:
         throw new Error(`unknown type: ${play.type}`)
     }
 
     // add volume credits
-    volumeCredits += Math.max(perf.audience - 30, 0)
+    volumeCredits += Math.max(perf['audience'] - 30, 0)
     // add extra credit for every ten comedy attendees
-    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5)
+    if (play.type === 'comedy')
+      volumeCredits += Math.floor(perf['audience'] / 5)
 
     // print line for this order
     result += `  ${play.name}: ${format(thisAmount / 100)} (${
-      perf.audience
+      perf['audience']
     } seats)\n`
     totalAmount += thisAmount
   }
