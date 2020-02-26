9f4b5e5dc4ef810039157e302fa06455ea0ef387
wpannell <wil.pannell@gmail.com>
2020-02-26 11:42:03 -0800

    inline play

--- src/martin-fowler-movie-example-2/statement.js
+++ src/martin-fowler-movie-example-2/statement.js
@@ -38,17 +38,16 @@ export function statement(invoice, plays) {
   }).format
 
   for (let perf of invoice['performances']) {
-    const play = playFor(perf)
-    let thisAmount = amountFor(perf, play)
+    let thisAmount = amountFor(perf, playFor(perf))
 
     // add volume credits
     volumeCredits += Math.max(perf['audience'] - 30, 0)
     // add extra credit for every ten comedy attendees
-    if (play.type === 'comedy')
+    if (playFor(perf).type === 'comedy')
       volumeCredits += Math.floor(perf['audience'] / 5)
 
     // print line for this order
-    result += `  ${play.name}: ${format(thisAmount / 100)} (${
+    result += `  ${playFor(perf).name}: ${format(thisAmount / 100)} (${
       perf['audience']
     } seats)\n`
     totalAmount += thisAmount
