0cfd12c1edd098111f80d7e27ee0df96a7000185
wpannell <wil.pannell@gmail.com>
2020-02-27 19:01:02 -0800

    print ‘first row cost’

--- src/martin-fowler-movie-example-2/html-statement.js
+++ src/martin-fowler-movie-example-2/html-statement.js
@@ -11,6 +11,7 @@ function renderHtml(config) {
     `<table>\n` +
     `<tr><th>play</th><th>seats</th><th>cost</th></tr>\n` +
     `  <tr><td>Hamlet</td><td>55</td>` +
+    `<td>$650.00</td></tr>\n` +
     `</table>\n` +
     `<p>You earned <em>${config.totalVolumeCredits}</em> credits</p>\n` +
     `<p>Amount owed is <em>${usd(config.total / 100)}</em></p>\n`
