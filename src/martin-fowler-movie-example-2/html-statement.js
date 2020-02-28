90d0b44c2b5e8b79f9c72672c3f54390dbe20c18
wpannell <wil.pannell@gmail.com>
2020-02-27 18:18:08 -0800

    print total in footer on htmlStatement()

--- src/martin-fowler-movie-example-2/html-statement.js
+++ src/martin-fowler-movie-example-2/html-statement.js
@@ -1,9 +1,13 @@
 import {configure} from './configure'
+import {usd} from './usd'
 
 export function htmlStatement(invoice, plays) {
   return renderHtml(configure(plays, invoice))
 }
 
 function renderHtml(config) {
-  return `<h1>Statement for ${config.customer}</h1>\n`
+  return (
+    `<h1>Statement for ${config.customer}</h1>\n` +
+    `<p>Amount owed is <em>${usd(config.total / 100)}</em></p>\n`
+  )
 }
