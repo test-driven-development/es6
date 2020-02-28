447d6d4c6e5d5824717a8cea49e2296397c11a63
wpannell <wil.pannell@gmail.com>
2020-02-27 19:05:41 -0800

    extract printRow()

--- src/martin-fowler-movie-example-2/html-statement.js
+++ src/martin-fowler-movie-example-2/html-statement.js
@@ -6,12 +6,15 @@ export function htmlStatement(invoice, plays) {
 }
 
 function renderHtml(config) {
+  function printRow() {
+    return `  <tr><td>Hamlet</td><td>55</td>` + `<td>$650.00</td></tr>\n`
+  }
+
   return (
     `<h1>Statement for ${config.customer}</h1>\n` +
     `<table>\n` +
     `<tr><th>play</th><th>seats</th><th>cost</th></tr>\n` +
-    `  <tr><td>Hamlet</td><td>55</td>` +
-    `<td>$650.00</td></tr>\n` +
+    printRow() +
     `</table>\n` +
     `<p>You earned <em>${config.totalVolumeCredits}</em> credits</p>\n` +
     `<p>Amount owed is <em>${usd(config.total / 100)}</em></p>\n`
