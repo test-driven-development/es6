452c9afbcf5ebd6ddbf829c20fab714ed88b112b
wpannell <wil.pannell@gmail.com>
2020-02-27 18:38:48 -0800

    print ‘table row after table start’

--- src/martin-fowler-movie-example-2/html-statement.js
+++ src/martin-fowler-movie-example-2/html-statement.js
@@ -9,6 +9,7 @@ function renderHtml(config) {
   return (
     `<h1>Statement for ${config.customer}</h1>\n` +
     `<table>\n` +
+    `<tr><th>play</th><th>seats</th><th>cost</th></tr>` +
     `</table>\n` +
     `<p>You earned <em>${config.totalVolumeCredits}</em> credits</p>\n` +
     `<p>Amount owed is <em>${usd(config.total / 100)}</em></p>\n`
