472618a4fdab5d8acf908e3ca127647c5a76ad6b
wpannell <wil.pannell@gmail.com>
2020-02-27 18:50:10 -0800

    print ‘first row play and seats’

--- src/martin-fowler-movie-example-2/html-statement.js
+++ src/martin-fowler-movie-example-2/html-statement.js
@@ -10,6 +10,7 @@ function renderHtml(config) {
     `<h1>Statement for ${config.customer}</h1>\n` +
     `<table>\n` +
     `<tr><th>play</th><th>seats</th><th>cost</th></tr>\n` +
+    `  <tr><td>Hamlet</td><td>55</td>` +
     `</table>\n` +
     `<p>You earned <em>${config.totalVolumeCredits}</em> credits</p>\n` +
     `<p>Amount owed is <em>${usd(config.total / 100)}</em></p>\n`
