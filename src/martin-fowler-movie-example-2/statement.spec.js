ef973ceefbf0f7d81a89815a84c768742316761b
wpannell <wil.pannell@gmail.com>
2020-02-27 20:00:44 -0800

    reverse order due to reduce

--- src/martin-fowler-movie-example-2/statement.spec.js
+++ src/martin-fowler-movie-example-2/statement.spec.js
@@ -63,8 +63,9 @@ describe(`html statement`, () => {
       `<h1>Statement for BigCo</h1>\n` +
       `<table>\n` +
       `<tr><th>play</th><th>seats</th><th>cost</th></tr>\n` +
-      `  <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>\n` +
       `  <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>\n` +
+      `  <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>\n` +
+      `  <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>\n` +
       `</table>\n` +
       `<p>Amount owed is <em>$1,730.00</em></p>\n` +
       `<p>You earned <em>47</em> credits</p>\n`
