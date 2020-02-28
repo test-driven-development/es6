42bb877be950332d2b36475272dc570140f75aa4
wpannell <wil.pannell@gmail.com>
2020-02-27 19:46:04 -0800

    fail test for entire statement

--- src/martin-fowler-movie-example-2/statement.spec.js
+++ src/martin-fowler-movie-example-2/statement.spec.js
@@ -57,4 +57,18 @@ describe(`html statement`, () => {
     const creditsFooter = `<p>You earned <em>47</em> credits</p>\n`
     htmlStatement(invoices[0], plays).should.containEql(creditsFooter)
   })
+
+  it(`prints the whole statement`, () => {
+    const statement =
+      `<h1>Statement for BigCo</h1>\n` +
+      `<table>\n` +
+      `<tr><th>play</th><th>seats</th><th>cost</th></tr>\n` +
+      `  <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>\n` +
+      `  <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>\n` +
+      `</table>\n` +
+      `<p>Amount owed is <em>$1,730.00</em></p>\n` +
+      `<p>You earned <em>47</em> credits</p>\n`
+
+    htmlStatement(invoices[0], plays).should.equal(statement)
+  })
 })
