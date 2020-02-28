8a42dd3cc1f36b110311f349399af40ce5947548
wpannell <wil.pannell@gmail.com>
2020-02-27 19:51:15 -0800

    correct output order in failed test

--- src/martin-fowler-movie-example-2/html-statement.js
+++ src/martin-fowler-movie-example-2/html-statement.js
@@ -20,7 +20,7 @@ function renderHtml(config) {
     `<tr><th>play</th><th>seats</th><th>cost</th></tr>\n` +
     printRow(config) +
     `</table>\n` +
-    `<p>You earned <em>${config.totalVolumeCredits}</em> credits</p>\n` +
-    `<p>Amount owed is <em>${usd(config.total / 100)}</em></p>\n`
+    `<p>Amount owed is <em>${usd(config.total / 100)}</em></p>\n` +
+    `<p>You earned <em>${config.totalVolumeCredits}</em> credits</p>\n`
   )
 }
