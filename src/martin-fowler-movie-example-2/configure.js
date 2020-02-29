7e2241c201b62ea09c3ee787956019aeddd2f3f1
wpannell <wil.pannell@gmail.com>
2020-02-28 21:01:05 -0800

    begin move method refactoring

--- src/martin-fowler-movie-example-2/configure.js
+++ src/martin-fowler-movie-example-2/configure.js
@@ -3,10 +3,34 @@ const shallowClone = obj => ({...obj})
 function performanceCalculator(performance_, play_) {
   const play = play_
   const performance = performance_
+  function amountFor(performance) {
+    let result = 0
+
+    switch (performance.play.type) {
+      case 'tragedy':
+        result = 40000
+        if (performance['audience'] > 30) {
+          result += 1000 * (performance['audience'] - 30)
+        }
+        break
+      case 'comedy':
+        result = 30000
+        if (performance['audience'] > 20) {
+          result += 10000 + 500 * (performance['audience'] - 20)
+        }
+        result += 300 * performance['audience']
+        break
+      default:
+        throw new Error(`unknown type: ${performance.play.type}`)
+    }
+
+    return result
+  }
 
   return {
     play,
     performance,
+    amount: amountFor(performance),
   }
 }
 
