7947e1f7bb5b02d5beb20704bed7e4f86f53a325
wpannell <wil.pannell@gmail.com>
2020-02-28 21:33:13 -0800

    rename / reduce numnber of elements

--- src/martin-fowler-movie-example-2/configure.js
+++ src/martin-fowler-movie-example-2/configure.js
@@ -5,43 +5,40 @@ function performanceCalculator(performance_, play_) {
   const performance = performance_
 
   function amountFor() {
-    let result = 0
+    let amount = 0
 
     switch (play.type) {
       case 'tragedy':
-        result = 40000
+        amount = 40000
         if (performance.audience > 30) {
-          result += 1000 * (performance.audience - 30)
+          amount += 1000 * (performance.audience - 30)
         }
         break
       case 'comedy':
-        result = 30000
+        amount = 30000
         if (performance['audience'] > 20) {
-          result += 10000 + 500 * (performance.audience - 20)
+          amount += 10000 + 500 * (performance.audience - 20)
         }
-        result += 300 * performance.audience
+        amount += 300 * performance.audience
         break
       default:
         throw new Error(`unknown type: ${play.type}`)
     }
 
-    return result
+    return amount
   }
-  function volumeCreditsFor(performance) {
-    let result = 0
-    result += Math.max(performance['audience'] - 30, 0)
-
-    if (play.type === 'comedy')
-      result += Math.floor(performance['audience'] / 5)
-
-    return result
+  function volumeCreditsFor() {
+    let credits = 0
+    credits += Math.max(performance.audience - 30, 0)
+    if (play.type === 'comedy') credits += Math.floor(performance.audience / 5)
+    return credits
   }
 
   return {
     play,
     performance,
     amount: amountFor(),
-    volumeCredits: volumeCreditsFor(performance),
+    volumeCredits: volumeCreditsFor(),
   }
 }
 
